c68c2276e0bc0653298c787cae0432062a3861b3
JScoder4005 <muk4005@gmail.com>
2025-02-11 12:05:24 +0530

    Created a new schmea/table in convex for add friends, conversation and group chats.

--- components/shared/sidebar/nav/MobileNav.tsx
+++ components/shared/sidebar/nav/MobileNav.tsx
@@ -21,40 +21,6 @@ const MobileNav = () => {
 
   if (isActive) return null;
   return (
-    // <Card className="fixed bottom-4 w-[calc(100vh-32px)] flex items-center h-16 p-2 lg:hidden">
-    //   <nav className="w-full">
-    //     <ul className="flex  items-center justify-evenly">
-    //       {paths.map((path, id) => {
-    //         return (
-    //           <li key={id} className="relative">
-    //             <Link href={path.href}>
-    //               <Tooltip>
-    //                 <TooltipTrigger>
-    //                   <Button
-    //                     size={'icon'}
-    //                     variant={path.active ? 'default' : 'outline'}
-    //                   >
-    //                     {path.icon}
-    //                   </Button>
-    //                 </TooltipTrigger>
-    //                 <TooltipContent>
-    //                   <p>{path.name}</p>
-    //                 </TooltipContent>
-    //               </Tooltip>
-    //             </Link>
-    //           </li>
-    //         );
-    //       })}
-    //       <li>
-    //         <ThemeToggle />
-    //       </li>
-    //       <li>
-    //         <UserButton />
-    //       </li>
-    //     </ul>
-    //   </nav>
-    // </Card>
-
     <Card className="fixed bottom-4 w-[calc(100vw-32px)] flex items-center h-16 p-2 lg:hidden overflow-visible z-50">
       <nav className="w-full">
         <ul className="flex items-center justify-between space-x-4">
